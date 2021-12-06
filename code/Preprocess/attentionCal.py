aa3e4cdf6f514551b8188115b9f5cb4e2722854c
Sayani <sayanik@andrew.cmu.edu>
2021-12-06 02:56:27 -0500

    Lenient and Conservative attention

--- code/Preprocess/attentionCal.py
+++ code/Preprocess/attentionCal.py
@@ -11,7 +11,7 @@ def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
- 
+
 def neg_softmax(x):
     """Compute softmax values for each sets of scores in x. Here we convert the exponentials to 1/exponentials"""
     e_x = np.exp(-(x - np.max(x)))
@@ -27,25 +27,33 @@ def aggregate_attention(at_mask,row,params):
     """input: attention vectors from 2/3 annotators (at_mask), row(dataframe row), params(parameters_dict)
        function: aggregate attention from different annotators.
        output: aggregated attention vector"""
-    
-    
+
+
     #### If the final label is normal or non-toxic then each value is represented by 1/len(sentences)
     if(row['final_label'] in ['normal','non-toxic']):
         at_mask_fin=[1/len(at_mask[0]) for x in at_mask[0]]
     else:
         at_mask_fin=at_mask
-        #### Else it will choose one of the options, where variance is added, mean is calculated, finally the vector is normalised.   
+        #### Else it will choose one of the options, where variance is added, mean is calculated, finally the vector is normalised.
         if(params['type_attention']=='sigmoid'):
             at_mask_fin=int(params['variance'])*at_mask_fin
-            at_mask_fin=np.mean(at_mask_fin,axis=0)
+            # at_mask_fin=np.mean(at_mask_fin,axis=0)
+            # at_mask_fin=np.prod(at_mask_fin,axis=0)
+            at_mask_fin = 1*(np.sum(at_mask_fin,axis=0)>0)
             at_mask_fin=sigmoid(at_mask_fin)
         elif (params['type_attention']=='softmax'):
             at_mask_fin=int(params['variance'])*at_mask_fin
-            at_mask_fin=np.mean(at_mask_fin,axis=0)
+            # at_mask_fin=np.mean(at_mask_fin,axis=0)
+            #or
+            print("using OR attention")
+            at_mask_fin = 1*(np.sum(at_mask_fin,axis=0)>0)
+            # at_mask_fin=np.prod(at_mask_fin,axis=0)
             at_mask_fin=softmax(at_mask_fin)
         elif (params['type_attention']=='neg_softmax'):
             at_mask_fin=int(params['variance'])*at_mask_fin
-            at_mask_fin=np.mean(at_mask_fin,axis=0)
+            # at_mask_fin=np.mean(at_mask_fin,axis=0)
+            # at_mask_fin=np.prod(at_mask_fin,axis=0)
+            at_mask_fin = 1*(np.sum(at_mask_fin,axis=0)>0)
             at_mask_fin=neg_softmax(at_mask_fin)
         elif(params['type_attention'] in ['raw','individual']):
             pass
@@ -53,27 +61,27 @@ def aggregate_attention(at_mask,row,params):
          at_mask_fin=decay(at_mask_fin,params)
 
     return at_mask_fin
-    
-    
-    
-##### Decay and distribution functions.To decay the attentions left and right of the attented word. This is done to decentralise the attention to a single word. 
+
+
+
+##### Decay and distribution functions.To decay the attentions left and right of the attented word. This is done to decentralise the attention to a single word.
 def distribute(old_distribution, new_distribution, index, left, right,params):
     window = params['window']
     alpha = params['alpha']
     p_value = params['p_value']
     method =params['method']
-    
-    
+
+
     reserve = alpha * old_distribution[index]
 #     old_distribution[index] = old_distribution[index] - reserve
-    
-    if method=='additive':    
+
+    if method=='additive':
         for temp in range(index - left, index):
             new_distribution[temp] = new_distribution[temp] + reserve/(left+right)
-        
+
         for temp in range(index + 1, index+right):
             new_distribution[temp] = new_distribution[temp] + reserve/(left+right)
-    
+
     if method == 'geometric':
         # we first generate the geometric distributio for the left side
         temp_sum = 0.0
@@ -83,10 +91,10 @@ def distribute(old_distribution, new_distribution, index, left, right,params):
             newprob.append(each_prob)
             temp_sum +=each_prob
             newprob = [each/temp_sum for each in newprob]
-        
+
         for temp in range(index - left, index):
             new_distribution[temp] = new_distribution[temp] + reserve*newprob[-(temp-(index-left))-1]
-        
+
         # do the same thing for right, but now the order is opposite
         temp_sum = 0.0
         newprob = []
@@ -97,7 +105,7 @@ def distribute(old_distribution, new_distribution, index, left, right,params):
             newprob = [each/temp_sum for each in newprob]
         for temp in range(index + 1, index+right):
             new_distribution[temp] = new_distribution[temp] + reserve*newprob[temp-(index + 1)]
-    
+
     return new_distribution
 
 
@@ -117,5 +125,4 @@ def decay(old_distribution,params):
         tempsum = sum(norm_distribution)
         new_distrbution = [each/tempsum for each in norm_distribution]
     return new_distribution
-
 
